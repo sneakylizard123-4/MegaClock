0033495bba781cb07acb4ad5e44a5382a689f447
pn2222a <sneakylizard123_4@hotmail.com>
2025-10-25 20:48:49 -0700

    .gitignore extensions.json include lib main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <TM1637Display.h>
+
+#define CLK_PIN 2
+#define DIO_PIN 3
+
+TM1637Display display(CLK_PIN, DIO_PIN);
+
+const uint8_t SEG_DONE[] = {
+	SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
+	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+	SEG_C | SEG_E | SEG_G,                           // n
+	SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
+	};
+
+const uint8_t SEG_TRUE[] = {
+  SEG_D | SEG_E | SEG_F | SEG_G,         // t
+  SEG_E | SEG_G,                         // r
+  SEG_C | SEG_D | SEG_E,                 // u
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G  // e
+  };
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("MegaClock Starting...");
+}
+
+void loop() {
+    // Main loop code here
+    display.setBrightness(0x0f); // Set maximum brightness
+    display.setSegments(SEG_DONE);
+    delay(1000);
+    display.setSegments(SEG_TRUE);
+    delay(1000);
+}
