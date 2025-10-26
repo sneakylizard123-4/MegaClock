00a3ce926c1a751c5c4559ea8bf263b72fc4364c
pn2222a <sneakylizard123_4@hotmail.com>
2025-10-26 11:01:04 -0700

    Ouch

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,21 @@ const uint8_t SEG_DONE[] = {
 	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
 	SEG_C | SEG_E | SEG_G,                           // n
 	SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
-	};
+};
 
 const uint8_t SEG_TRUE[] = {
   SEG_D | SEG_E | SEG_F | SEG_G,         // t
   SEG_E | SEG_G,                         // r
   SEG_C | SEG_D | SEG_E,                 // u
   SEG_A | SEG_D | SEG_E | SEG_F | SEG_G  // e
-  };
+};
+
+const uint8_t SEG_OUCH[] = {
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,// O
+  SEG_C | SEG_D | SEG_E,                        // u
+  SEG_D | SEG_E | SEG_G,                        // c
+  SEG_C | SEG_E | SEG_F | SEG_G                 // h
+};
 
 void setup() {
     Serial.begin(9600);
@@ -31,5 +38,7 @@ void loop() {
     display.setSegments(SEG_DONE);
     delay(1000);
     display.setSegments(SEG_TRUE);
+    delay(1000);
+    display.setSegments(SEG_OUCH);
     delay(1000);
 }
